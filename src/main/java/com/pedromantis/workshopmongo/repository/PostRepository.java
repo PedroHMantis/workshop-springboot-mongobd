ca951a14ad6285ca6d62c8a052f1cefd6c37c9d0
PedroHMantis <gtx1050pd@gmail.com>
2025-09-06 13:20:05 -0300

    Consulta simples com query methods e mongo regex

--- src/main/java/com/pedromantis/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/pedromantis/workshopmongo/repository/PostRepository.java
@@ -2,10 +2,14 @@ package com.pedromantis.workshopmongo.repository;
 
 import com.pedromantis.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post,String> {
+
+    @Query("{'title': {$regex: ?0, $options: 'i'} }")
+    List<Post> findByTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
