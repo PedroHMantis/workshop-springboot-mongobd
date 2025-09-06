6970a5553163b272824cb59d30efc9923da794f3
PedroHMantis <gtx1050pd@gmail.com>
2025-09-06 12:24:22 -0300

    Referenciando os posts do usuário

--- src/main/java/com/pedromantis/workshopmongo/config/Intantiation.java
+++ src/main/java/com/pedromantis/workshopmongo/config/Intantiation.java
@@ -34,12 +34,15 @@ public class Intantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-        
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
         Post post1 = new Post(null, sdf.parse("27/09/2025"),"Partiu Viagem","Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("27/10/2026"),"Bom dia","Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
